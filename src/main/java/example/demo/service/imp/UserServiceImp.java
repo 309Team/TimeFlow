ac086a37cf53024edcf0c140aa2f79be0bd8f30f
Polister <yyla05@foxmail.com>
2023-10-22 15:50:40 +0800

    完成JDBC与MyBatis连接，修改application.yml配置即可。

--- src/main/java/example/demo/service/imp/UserServiceImp.java
+++ src/main/java/example/demo/service/imp/UserServiceImp.java
@@ -1,8 +1,14 @@
 package example.demo.service.imp;
 
 
+import example.demo.mapper.UserMapper;
+import example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImp {
+public class UserServiceImp implements UserService {
+    @Autowired
+    UserMapper userMapper;
+
 }
