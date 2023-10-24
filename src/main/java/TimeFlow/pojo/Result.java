14cc8140685a10127241e1dd1753cf770effc5f7
Polister <yyla05@foxmail.com>
2023-10-24 22:35:20 +0800

    修改结果类，可以自定义错误码了。

--- src/main/java/TimeFlow/pojo/Result.java
+++ src/main/java/TimeFlow/pojo/Result.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Result {
-	private Boolean code; //Ture表示成功，False表示失败
+    private Integer code; //0表示成功，其他表示失败
 	private String msg; //返回信息
 	private Object data; //返回对象列表
 
@@ -19,7 +19,7 @@ public class Result {
 	 * @return Result对象
 	 */
 	static public Result success(Object data) {
-		return new Result(true, "Success", data);
+        return new Result(0, "Success", data);
 	}
 
 	/**
@@ -28,7 +28,7 @@ public class Result {
 	 * @return Result对象
 	 */
 	static public Result success() {
-		return new Result(true, "Success", null);
+        return new Result(0, "Success", null);
 	}
 
 	/**
@@ -38,7 +38,18 @@ public class Result {
 	 * @return Result对象
 	 */
 	static public Result error(String msg) {
-		return new Result(false, msg, null);
+        return new Result(1, msg, null);
 	}
+
+    /**
+     * 错误返回（自定义代码）
+     *
+     * @param code 错误码
+     * @param msg  错误信息
+     * @return Result对象
+     */
+    static public Result err(Integer code, String msg) {
+        return new Result(code, msg, null);
+    }
 
 }
