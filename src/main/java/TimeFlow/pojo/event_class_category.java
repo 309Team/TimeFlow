818582e33aaf362d35429c288eae80e6a9402b08
ZRCHOU <zhengruchou@outlook.com>
2023-10-27 17:54:59 +0800

    添加映射pojo

---
+++ src/main/java/TimeFlow/pojo/event_class_category.java
@@ -0,0 +1,15 @@
+package TimeFlow.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class event_class_category {
+	private Integer id;//关系映射id
+	private Integer id_event;//事项id
+	private Integer id_class;//类别id
+}
